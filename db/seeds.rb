b9c9643b6a404f17e01d587b15abdb4d3f02e153
Arnau Izard Torredemer <arnau.izardtorredemer@solvaypostgrad.net>
2018-06-13 16:40:23 +0200

    advanced routing done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'japanese',
+    phone_number: '656 546 543'
+
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'belgian',
+    phone_number: '656 546 543'
+  },
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'japanese',
+    phone_number: '656 546 543'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'belgian',
+    phone_number: '656 546 543'
+  },
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'japanese',
+    phone_number: '656 546 543'
+  }
+
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
